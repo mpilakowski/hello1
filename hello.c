ffd27544b52b7ab3ff53fc86a1b47f2c79974065
Isa-Adrover <59894527+Isa-Adrover@users.noreply.github.com>
2020-01-16 11:52:42 -0600

    Update hello1
    Added my major and my name

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ Date: 01/16/20
 #include <stdio.h>
 int main(void) {
   printf("Maverick Pilakowski\n");
-  printf("Mechanical Engineer\n")
+  printf("Mechanical Engineer\n");
+  printf("Isabel Adrover Gallego\n");
+  printf("Undeclared\n");
   return 0;
 }
